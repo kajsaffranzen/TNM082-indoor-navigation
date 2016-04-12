e35e61cb531937fe2d4b2bff04ea43458b2a45c2
Rickard Lindstedt <rickard.lindstedt@gmail.com>
2016-04-12 13:43:42 +0200

    reading from firebase

--- app/src/main/java/com/example/firebazzze/tnm082_indoor_navigation/MainActivity.java
+++ app/src/main/java/com/example/firebazzze/tnm082_indoor_navigation/MainActivity.java
@@ -5,15 +5,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
+import com.firebase.client.DataSnapshot;
+
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -23,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i("kalle", "wooorking");
+
+                Firebase DB = new Firebase("https://kalle.firebaseio.com/");
+
+                DB.child("Tappan").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        Log.i("kalle", "heeeerreeeee");
+                        System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."'
+                        Log.i("kalle", "heeeerreeeee2323223");
+                    }
+                    @Override public void onCancelled(FirebaseError error) { }
+                });
+
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
